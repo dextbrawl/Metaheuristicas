91c9316af1dfff6c416a7d913c9777d2285eb0e7
Sgatanasio <i32gaats@uco.es>
2026-02-17 23:51:26 +0100

    First prototype of Random Search idk if it works

--- BusquedaAleatoria.py
+++ BusquedaAleatoria.py
@@ -5,34 +5,32 @@ from sklearn.metrics import mean_squared_error
 
 # Funciones auxiliares
 
-def calcular_MSE_segmento(puntos_segmento):
-    puntos_segmento = np.array(puntos_segmento)
-    n = len(puntos_segmento)
+def segmentMSE(breaking_points):
+    breaking_points = np.array(breaking_points)
+    n = len(breaking_points)
     X = np.arange(n).reshape(-1, 1) # Para ponerlo en forma de columna y que lo pille sklearn
-    y = puntos_segmento.reshape(-1, 1)
+    y = breaking_points.reshape(-1, 1)
 
-    modelo = LinearRegression()
-    modelo.fit(X, y)
-    prediccion = modelo.predict(X)
+    model = LinearRegression()
+    model.fit(X, y)
+    prediction = model.predict(X)
 
-    mse = mean_squared_error(y, prediccion)
+    mse = mean_squared_error(y, prediction)
 
     return mse
 
-def calcular_RMSE(serie_temporal, limites_segmentos):
-    Ndatos = len(serie_temporal)
-    serie_completa = [0] + limites_segmentos + [Ndatos]
-    errores_segmento = []
+def avgMSE(temp_series, breaking_points):
+    segment_errs = []
 
-    for i in range(len(serie_completa) - 1):
-        inicio = serie_completa[i]
-        fin = serie_completa[i+1]
+    for i in range(len(temp_series) - 1):
+        inicio = temp_series[i]
+        fin = temp_series[i+1]
 
-        segmento = serie_temporal[inicio:fin]
-        mse_segmento = calcular_MSE_segmento(segmento)
-        errores_segmento.append(mse_segmento)
+        segment = temp_series[inicio:fin]
+        segment_mse = segmentMSE(segment)
+        segment_errs.append(segment_mse)
 
-    mse_mean = np.mean(errores_segmento)
+    mse_mean = np.mean(segment_errs)
 
     return mse_mean
 
@@ -41,19 +39,27 @@ def readSeries(filename) -> list: # Función para crear la lista que define la s
     return data
 
 def getBreakingPoints(n_points, k_segments): # Función de búsqueda aleatoria
-    
     breaking_points = [0,n_points-1]
 
     for _ in range(k_segments -1):
         n = random.randint(0,n_points-1)
-        
         while n in breaking_points:
             n = random.randint(0,n_points-1)
-        
         breaking_points.append(n)
-    
     breaking_points.sort()
 
     return breaking_points
 
 def randomSearch(series: list, k_segments):
+    size = len(series)
+
+    breaking_points = getBreakingPoints(size,k_segments)
+    avg_mse = avgMSE(series,breaking_points)
+
+    for _ in range(100000):
+        new_breaking_points = getBreakingPoints(size, k_segments)
+        new_avg_mse = avgMSE(series,breaking_points)
+
+        if new_avg_mse < avg_mse :
+            breaking_points = new_breaking_points
+            avg_mse = new_avg_mse
