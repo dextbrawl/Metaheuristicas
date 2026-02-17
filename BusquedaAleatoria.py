0e1a1601a26213b4772dcf27fb9fc740580e09a7
ByteKata08 <melladoarenasjuan@gmail.com>
2026-02-17 19:09:54 +0100

    funciones de MSE de cada segmento y MSE de la serie completa implementadas(no probadas)

--- BusquedaAleatoria.py
+++ BusquedaAleatoria.py
@@ -1,5 +1,41 @@
 import random
 import numpy as np
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Funciones auxiliares
+
+def calcular_MSE_segmento(puntos_segmento):
+    puntos_segmento = np.array(puntos_segmento)
+    n = len(puntos_segmento)
+    X = np.arange(n).reshape(-1, 1) # Para ponerlo en forma de columna y que lo pille sklearn
+    y = puntos_segmento.reshape(-1, 1)
+
+    modelo = LinearRegression()
+    modelo.fit(X, y)
+    prediccion = modelo.predict(X)
+
+    mse = mean_squared_error(y, prediccion)
+
+    return mse
+
+def calcular_RMSE(serie_temporal, limites_segmentos):
+    Ndatos = len(serie_temporal)
+    serie_completa = [0] + limites_segmentos + [Ndatos]
+    errores_segmento = []
+
+    for i in range(len(serie_completa) - 1):
+        inicio = serie_completa[i]
+        fin = serie_completa[i+1]
+
+        segmento = serie_temporal[inicio:fin]
+        mse_segmento = calcular_MSE_segmento(segmento)
+        errores_segmento.append(mse_segmento)
+
+    mse_mean = np.mean(errores_segmento)
+
+    return mse_mean
 
 def readSeries(filename) -> list: # Función para crear la lista que define la serie
     data = np.loadtxt(filename).tolist()
@@ -21,9 +57,4 @@ def getBreakingPoints(n_points, k_segments): # Función de búsqueda aleatoria
 
     return breaking_points
 
-def avgError(series: list, start_point, end_point): # Función para calcular el error medio de cada segmento
-    
-
 def randomSearch(series: list, k_segments):
-
-    
