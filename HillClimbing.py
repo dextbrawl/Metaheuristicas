fb2eaaceaa185e14d4497bac6fa3b6d6c52fe8c3
ByteKata08 <melladoarenasjuan@gmail.com>
2026-02-21 20:15:46 +0100

    adding statistics methods to hillclimbing and creation o main.py file for the centralization and make even easier to use the code

--- HillClimbing.py
+++ HillClimbing.py
@@ -1,9 +1,12 @@
+from re import error
+from numpy import append
 import metrics as me
 def print_segments(breaking_points):
     for i in range(len(breaking_points) - 1):
         print(f'[{breaking_points[i]}][{breaking_points[i+1]}]')
 
 def hillClimbingSearch(series, k_segments, prev_breaking_points): # Sacar nuevos puntos vecinos que sean mejores que los anteriores
+    errors = []
     improved = True 
     while improved:
         i = 1
@@ -26,20 +29,31 @@ def hillClimbingSearch(series, k_segments, prev_breaking_points): # Sacar nuevos
             if mean_mse_inc < prev_mean_mse and mean_mse_dec >= prev_mean_mse: # Mejora incrementando y empeora decrementando
                 prev_breaking_points = inc_breaking_points
                 improved = True
+                errors.append(mean_mse_inc)
 
             elif mean_mse_inc >= prev_mean_mse and mean_mse_dec < prev_mean_mse: # Mejora decrementando y empeora incrementando
                 prev_breaking_points = dec_breaking_points
                 improved = True
+                errors.append(mean_mse_dec)
 
             elif mean_mse_inc < prev_mean_mse and mean_mse_dec < prev_mean_mse: # Mejora en los dos casos
                 if mean_mse_inc < mean_mse_dec: #Mejora más incrementando
                     prev_breaking_points = inc_breaking_points
                     improved = True
+                    errors.append(mean_mse_inc)
                 else: # Mejora más (o igual) decrementando
                     prev_breaking_points = dec_breaking_points
                     improved = True
+                    errors.append(mean_mse_dec)
             else: # Empeora en ambos casos
                 i += 1
+    errors_mean = me.calculateErrorMean(errors)
+    print(f"Average of errors: ", errors_mean)
+    error_variance = me.calculateVariance(errors)
+    print(f"variance of errors: ", error_variance)
+    standard_desviation = me.calculateStandardDesviation(errors)
+    print(f"Standard desviation of errors: ", standard_desviation)
+
     return prev_breaking_points
 
 series = me.readSeries('TS1.txt')
