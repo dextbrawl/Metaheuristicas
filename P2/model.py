c7d2b6bdd5ec7b5256de20775dd10429076ce9cb
ByteKata08 <melladoarenasjuan@gmail.com>
2026-03-17 12:51:24 +0100

    update

--- P2/model.py
+++ P2/model.py
@@ -1,31 +1,63 @@
-import pandas as pd
 import numpy as np
-
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 
 # cargar dataset
-data = pd.read_csv("winequality-red.csv", sep=';')
+data = pd.read_csv("winequality-red.csv", sep=";")
 
 # convertir problema a clasificación binaria
 data["quality"] = (data["quality"] >= 6).astype(int)
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
-def evaluate_solution(params):
+
+class Individual:
+    def __init__(
+        self,
+        n_estimators,
+        max_depth,
+        min_samples_split,
+        min_samples_leaf,
+        max_features,
+        bootstrap,
+        criterion,
+        class_weight,
+        max_leaf_nodes,
+        min_impurity_decrease,
+        random_state,
+    ):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.max_features = max_features
+        self.bootstrap = bootstrap
+        self.criterion = criterion
+        self.class_weight = class_weight
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+        self.random_state = random_state
+
+
+def evaluate_solution(ind):
     model = RandomForestClassifier(
-        n_estimators=int(params[0]),
-        max_depth=int(params[1]),
-        min_samples_split=int(params[2]),
-        min_samples_leaf=int(params[3]),
-        max_features=float(params[4]),
-        bootstrap=bool(params[5]),
-        criterion="gini" if params[6] == 0 else "entropy",
-        class_weight=None if params[7] == 0 else "balanced",
-        max_leaf_nodes=int(params[8]),
-        min_impurity_decrease=float(params[9]),
-        random_state=42
+        n_estimators=int(ind.n_estimators),
+        max_depth=int(ind.max_depth),
+        min_samples_split=int(ind.min_samples_split),
+        min_samples_leaf=int(ind.min_samples_leaf),
+        max_features=float(ind.max_features),
+        bootstrap=bool(ind.bootstrap),
+        criterion="gini" if ind.criterion == 0 else "entropy",
+        class_weight=None if ind.class_weight == 0 else "balanced",
+        max_leaf_nodes=int(ind.max_leaf_nodes),
+        min_impurity_decrease=float(ind.min_impurity_decrease),
+        random_state=int(ind.random_state),
     )
     scores = cross_val_score(model, X, y, cv=5, scoring="accuracy")
     return scores.mean()
+
+
+if __name__ == "__main__":
+    params_obj = Individual(10, 2, 2, 1, 0.1, 0, 0, 0, 10, 0, 42)
+    print(f"Accuracy promedio: {evaluate_solution(params_obj)}")
