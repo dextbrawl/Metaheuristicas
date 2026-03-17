81f3c3dcee0c43802b0d2df701cd93538be69779
Sgatanasio <i32gaats@uco.es>
2026-03-17 12:22:48 +0100

    first model + csv reading

--- P2/model.py
+++ P2/model.py
@@ -1,72 +1,31 @@
-import random
-import Selection as sel
-import CreatePopulation as pop
-import individuals as ind
-import numpy as np
 import pandas as pd
+import numpy as np
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 
 # cargar dataset
-data = pd.read_csv("winequality-red.csv")
+data = pd.read_csv("winequality-red.csv", sep=';')
 
 # convertir problema a clasificación binaria
 data["quality"] = (data["quality"] >= 6).astype(int)
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
-
-# En una poblacion, cada individuo tiene su score.
-class Population:
-    def __init__(self, individual):
-        self.IndividualAndItScore = []
-
-        for ind in individual:
-            score = evaluate_solution(ind)
-            self.IndividualAndItScore.append((ind, score))
-
-
-def evaluate_solution(ind: ind.Individual):
+def evaluate_solution(params):
     model = RandomForestClassifier(
-        n_estimators=int(ind.n_estimators),
-        max_depth=int(ind.max_depth),
-        min_samples_split=int(ind.min_samples_split),
-        min_samples_leaf=int(ind.min_samples_leaf),
-        max_features=float(ind.max_features),
-        bootstrap=bool(ind.bootstrap),
-        criterion="gini" if ind.criterion == 0 else "entropy",
-        class_weight=None if ind.class_weight == 0 else "balanced",
-        max_leaf_nodes=int(ind.max_leaf_nodes),
-        min_impurity_decrease=float(ind.min_impurity_decrease),
-        random_state=int(ind.random_state),
+        n_estimators=int(params[0]),
+        max_depth=int(params[1]),
+        min_samples_split=int(params[2]),
+        min_samples_leaf=int(params[3]),
+        max_features=float(params[4]),
+        bootstrap=bool(params[5]),
+        criterion="gini" if params[6] == 0 else "entropy",
+        class_weight=None if params[7] == 0 else "balanced",
+        max_leaf_nodes=int(params[8]),
+        min_impurity_decrease=float(params[9]),
+        random_state=42
     )
     scores = cross_val_score(model, X, y, cv=5, scoring="accuracy")
     return scores.mean()
-
-
-if __name__ == "__main__":
-    PopulationSize = 20  # Parametro
-
-    print("Formas de incializar nuestra poblacion:")
-    print("  1. Random")
-    print("  2. Secuencial")
-
-    while True:
-        try:
-            metodo = int(input("\nElige un método (1 o 2): "))
-            if metodo in [1, 2]:
-                break
-            else:
-                print("Tiene que ser 1 o 2")
-        except ValueError:
-            print("Tiene que ser 1 o 2.")
-
-    # Generar población según el método elegido
-    if metodo == 1:
-        individuos = pop.CreateRandomPopulation(PopulationSize)
-        print(f"Población aleatoria creada con {len(individuos)} individuos")
-    else:
-        Min = 0.15
-        MaxTry = 100
-        individuos = pop.CreateSequentialPopulation(PopulationSize, Min, MaxTry)
-        print(f"Población secuencial creada con {len(individuos)} individuos")
