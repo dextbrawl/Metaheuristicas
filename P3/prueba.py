04d3c03a1bba7c0d0d06c3a70e6bd08ba2c3b99d
Antonio Jesús Merlo Morales <antoniojesusmerlomorales10@gmail.com>
2026-04-21 12:18:45 +0200

    Creación de la práctica 3

---
+++ P3/prueba.py
@@ -0,0 +1,14 @@
+import joblib
+import numpy as np
+class BlackBoxModel:
+    #Considerar blackbox_modelA y blackbox_modelB
+    def __init__(self, path="blackbox_model.pkl"):
+        self.model = joblib.load(path)
+    def predict(self, x):
+        x = np.array(x).reshape(1, -1)
+        return self.model.predict(x)[0]
+    
+if __name__ == "__main__":
+    model = BlackBoxModel("blackbox_modelA.pkl")
+    x = [0.5, 0.2]
+    print(model.predict(x))
